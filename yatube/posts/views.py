c88e05fbd6fb90d1c920f82f9cb879e315e293f8
Алексеев Андрей <wembeded@gmail.com>
2023-02-28 21:52:30 +0300

    repair 28.02.2023 21:53

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,13 @@
-# from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post, Group
 
 
+POST_NUM = 10
+
+
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:POST_NUM]
     title = 'Последние обновления на сайте'
     context = {
         'posts': posts,
@@ -15,13 +18,13 @@ def index(request):
 
 
 def group_posts(request, slug):
-    group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    gr_db = Group.objects.first()
+    group_none = get_object_or_404(Group, slug=slug)
+    posts = gr_db.group_posts.all().order_by('-pub_date')[:POST_NUM]
     title = 'Посты сообщества.'
     context = {
-        'group': group,
+        'group': group_none,
         'posts': posts,
         'title': title,
-        'text': 'Лев Толстой — зеркало русской революции.',
     }
     return render(request, 'posts/group_list.html', context, slug)
